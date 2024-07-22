ac6865052f1ecfa0d48c4b3c75c7db7ab5eb35a8
Lucas Enoki <enoki.lucas@gmail.com>
2024-07-22 12:46:33 -0300

    mistake

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,6 +1,3 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails"
 import "controllers"
-
-import '@spectrum-web-components/theme/sp-theme.js';
-import '@spectrum-web-components/theme/src/themes.js';
--- config/importmap.rb
+++ config/importmap.rb
@@ -5,6 +5,3 @@ pin "@hotwired/turbo-rails", to: "turbo.min.js"
 pin "@hotwired/stimulus", to: "stimulus.min.js"
 pin "@hotwired/stimulus-loading", to: "stimulus-loading.js"
 pin_all_from "app/javascript/controllers", under: "controllers"
-
-pin '@spectrum-web-components/theme/sp-theme.js', to: '../node_modules/spectrum-web-components/theme/sp-theme.js'
-pin '@spectrum-web-components/theme/src/themes.js', to: '../node_modules/spectrum-web-components/theme/src/theme.js'
