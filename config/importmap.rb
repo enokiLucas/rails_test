1ef39d7a595d4867689812b7dc2c8104556c41fa
Lucas Enoki <enoki.lucas@gmail.com>
2024-07-22 12:34:54 -0300

    Try to use importmaps

--- config/importmap.rb
+++ config/importmap.rb
@@ -5,3 +5,6 @@ pin "@hotwired/turbo-rails", to: "turbo.min.js"
 pin "@hotwired/stimulus", to: "stimulus.min.js"
 pin "@hotwired/stimulus-loading", to: "stimulus-loading.js"
 pin_all_from "app/javascript/controllers", under: "controllers"
+
+pin '@spectrum-web-components/theme/sp-theme.js', to: '../node_modules/spectrum-web-components/theme/sp-theme.js'
+pin '@spectrum-web-components/theme/src/themes.js', to: '../node_modules/spectrum-web-components/theme/src/theme.js'
