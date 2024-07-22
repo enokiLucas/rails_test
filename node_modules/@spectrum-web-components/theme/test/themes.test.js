9c78781b5c5baf3d8112625d9162e957c9349e11
Lucas Enoki <enoki.lucas@gmail.com>
2024-07-21 21:11:37 -0300

    install adobe spectrum theme

---
+++ node_modules/@spectrum-web-components/theme/test/themes.test.js
@@ -0,0 +1,206 @@
+"use strict";
+import "@spectrum-web-components/theme/sp-theme.js";
+import "@spectrum-web-components/theme/src/themes.js";
+import { Theme } from "@spectrum-web-components/theme";
+import { elementUpdated, expect, fixture, html } from "@open-wc/testing";
+import { css } from "@spectrum-web-components/base";
+describe("Systems", () => {
+  it("loads - light", async () => {
+    const el = await fixture(
+      html`
+                <sp-theme system="spectrum" color="light"></sp-theme>
+            `
+    );
+    await elementUpdated(el);
+    expect(el).to.exist;
+    expect(el).shadowDom.to.exist;
+  });
+  it("loads - dark", async () => {
+    const el = await fixture(
+      html`
+                <sp-theme color="dark"></sp-theme>
+            `
+    );
+    await elementUpdated(el);
+    expect(el).to.exist;
+    expect(el).shadowDom.to.exist;
+  });
+  it("loads - unkown", async () => {
+    const el = await fixture(
+      html`
+                <sp-theme color="unknown" scale="unknown"></sp-theme>
+            `
+    );
+    await elementUpdated(el);
+    expect(el).to.exist;
+    expect(el).shadowDom.to.exist;
+  });
+  it("adds an instance only once", async () => {
+    const el = await fixture(
+      html`
+                <sp-theme system="express"></sp-theme>
+            `
+    );
+    await elementUpdated(el);
+    const testableTheme = Theme;
+    expect(testableTheme.instances.has(el), "first").to.be.true;
+    expect(testableTheme.instances.size).to.equal(1);
+    el.remove();
+    expect(testableTheme.instances.has(el), "second").to.be.false;
+    expect(testableTheme.instances.size).to.equal(0);
+    document.body.append(el);
+    expect(testableTheme.instances.has(el), "third").to.be.true;
+    expect(testableTheme.instances.size).to.equal(1);
+  });
+});
+describe("Lightest", () => {
+  it("loads", async () => {
+    const el = await fixture(
+      html`
+                <sp-theme color="lightest"></sp-theme>
+            `
+    );
+    await elementUpdated(el);
+    expect(el).to.exist;
+    expect(el).shadowDom.to.exist;
+  });
+});
+describe("Medium", () => {
+  it("loads", async () => {
+    const el = await fixture(
+      html`
+                <sp-theme scale="medium"></sp-theme>
+            `
+    );
+    await elementUpdated(el);
+    expect(el).to.exist;
+    expect(el).shadowDom.to.exist;
+  });
+});
+describe("App styles", () => {
+  it("applies app fragments", async () => {
+    const el = await fixture(
+      html`
+                <sp-theme color="light">
+                    <style>
+                        div {
+                            padding: var(--app-padding);
+                        }
+                    </style>
+                    <div></div>
+                </sp-theme>
+            `
+    );
+    const div = el.querySelector("div");
+    await elementUpdated(el);
+    const preStylesDiv = getComputedStyle(div);
+    expect(preStylesDiv.paddingBlockStart).to.equal("0px");
+    Theme.registerThemeFragment(
+      "app",
+      "app",
+      css`
+                :host {
+                    --app-padding: 10px;
+                }
+            `
+    );
+    await elementUpdated(el);
+    const postStylesDiv = getComputedStyle(div);
+    expect(postStylesDiv.paddingBlockStart).to.equal("10px");
+    Theme.themeFragmentsByKind.delete("app");
+  });
+});
+describe("Setting attributes", () => {
+  it("loads", async () => {
+    const el = await fixture(
+      html`
+                <sp-theme color="light"></sp-theme>
+            `
+    );
+    await elementUpdated(el);
+    expect(el).to.not.be.undefined;
+    expect(el.hasAttribute("scale")).to.be.false;
+    if (el.shadowRoot.adoptedStyleSheets) {
+      expect(el.shadowRoot.adoptedStyleSheets.length).to.equal(2);
+    } else {
+      expect(
+        [...el.shadowRoot.querySelectorAll("style")].length
+      ).to.equal(2);
+    }
+    await elementUpdated(el);
+    el.setAttribute("scale", "fish");
+    expect(el.getAttribute("scale")).to.equal("medium");
+    if (el.shadowRoot.adoptedStyleSheets) {
+      expect(el.shadowRoot.adoptedStyleSheets.length).to.equal(2);
+    } else {
+      expect(
+        [...el.shadowRoot.querySelectorAll("style")].length
+      ).to.equal(2);
+    }
+    el.color = "dark";
+    el.scale = "medium";
+    await elementUpdated(el);
+    expect(el.getAttribute("color")).to.equal("dark");
+    expect(el.getAttribute("scale")).to.equal("medium");
+    if (el.shadowRoot.adoptedStyleSheets) {
+      expect(el.shadowRoot.adoptedStyleSheets.length, "all").to.equal(3);
+    } else {
+      expect(
+        [...el.shadowRoot.querySelectorAll("style")].length
+      ).to.equal(3);
+    }
+    el.setAttribute("color", "fish");
+    await elementUpdated(el);
+    expect(el.getAttribute("color")).to.equal("dark");
+    if (el.shadowRoot.adoptedStyleSheets) {
+      expect(el.shadowRoot.adoptedStyleSheets.length, "last").to.equal(3);
+    } else {
+      expect(
+        [...el.shadowRoot.querySelectorAll("style")].length
+      ).to.equal(3);
+    }
+  });
+  it("prefers system over theme", async () => {
+    const el = await fixture(
+      html`
+                <sp-theme system="express"></sp-theme>
+            `
+    );
+    await elementUpdated(el);
+    expect(el.system).to.equal("express");
+    el.setAttribute("theme", "classic");
+    await elementUpdated(el);
+    expect(el.system).to.equal("express");
+    expect(el.theme).to.equal("express");
+  });
+});
+describe("Themes", () => {
+  it("updates system value even if only theme is added to sp-theme", async () => {
+    const el = await fixture(
+      html`
+                <sp-theme theme="express"></sp-theme>
+            `
+    );
+    await elementUpdated(el);
+    expect(el.theme).to.equal("express");
+    el.setAttribute("theme", "spectrum");
+    await elementUpdated(el);
+    expect(el.theme).to.equal("spectrum");
+    expect(el.system).to.equal("spectrum");
+  });
+  it("updates system and theme value iif both are added to sp-theme", async () => {
+    const el = await fixture(
+      html`
+                <sp-theme system="spectrum" theme="spectrum"></sp-theme>
+            `
+    );
+    await elementUpdated(el);
+    expect(el.theme).to.equal("spectrum");
+    expect(el.system).to.equal("spectrum");
+    el.setAttribute("theme", "express");
+    await elementUpdated(el);
+    expect(el.theme).to.equal("express");
+    expect(el.system).to.equal("express");
+  });
+});
+//# sourceMappingURL=themes.test.js.map
