a6237320827dd90d7f8d2f4f0d383a55a1d2cb61
Lucas Enoki <enoki.lucas@gmail.com>
2024-07-22 12:26:23 -0300

    Added importmaps for spectrum

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,8 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails"
 import "controllers"
+
+// Spectrum WebComponents
+/// Theme
+import "@spectrum-web-components/theme/sp-theme.js";
+import "@spectrum-web-components/theme/src/themes.js";
--- config/importmap.rb
+++ config/importmap.rb
@@ -5,3 +5,11 @@ pin "@hotwired/turbo-rails", to: "turbo.min.js"
 pin "@hotwired/stimulus", to: "stimulus.min.js"
 pin "@hotwired/stimulus-loading", to: "stimulus-loading.js"
 pin_all_from "app/javascript/controllers", under: "controllers"
+
+# Spectrum Web Components
+pin "@spectrum-web-components/theme", to: "https://cdn.jsdelivr.net/npm/@spectrum-web-components/theme@0.44.0/sp-theme.js"
+pin "@spectrum-web-components/theme/sp-theme.js", to: "https://cdn.jsdelivr.net/npm/@spectrum-web-components/theme@0.44.0/sp-theme.js"
+pin "@spectrum-web-components/theme/src/themes.js", to: "https://cdn.jsdelivr.net/npm/@spectrum-web-components/theme@0.44.0/src/themes.js"
+
+pin "@spectrum-web-components/base", to: "https://unpkg.com/@spectrum-web-components/base@0.44.0"
+pin "@spectrum-web-components/base/condition-attribute-with-id.js", to: "https://unpkg.com/@spectrum-web-components/base@0.44.0/condition-attribute-with-id.js"
