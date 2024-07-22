655b2484313a415515bc01883127487270ab8121
Lucas Enoki <enoki.lucas@gmail.com>
2024-07-22 14:04:12 -0300

    more tries

--- config/importmap.rb
+++ config/importmap.rb
@@ -7,5 +7,3 @@ pin "@hotwired/stimulus-loading", to: "stimulus-loading.js"
 pin_all_from "app/javascript/controllers", under: "controllers"
 
 # Spectrum Web Components
-pin "@spectrum-web-components/theme", to: "@spectrum-web-components--theme.js" # @0.44.0
-pin "@lit/reactive-element", to: "@lit--reactive-element.js" # @2.0.4
